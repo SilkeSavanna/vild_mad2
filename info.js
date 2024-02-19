588e32db7a28872055a21dccf7da1e35ba2035fa
Nikolaj <nikolajgottschalck@hotmail.com>
2024-02-19 12:52:55 +0100

    små rettelser i css

--- info.js
+++ info.js
@@ -2,9 +2,9 @@ const urlParams = new URLSearchParams(window.location.search);
 const id = urlParams.get("id");
 
 
-/* fetch("https://kea-alt-del.dk/t7/api/products/" + id) */
-/* .then((res)=>res.json())
-.then((data) => showProduct(data)); */
+fetch("https://lzlmyauwovywksmqbdpd.supabase.co" + id)
+.then((res)=>res.json())
+.then((data) => showProduct(data));
 
 function showProduct(product){
     console.log(product);
