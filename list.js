fc9c7216995d86ffea7a62453b1da27721888ec0
SilkeSavanna <sihe0002@stud.kea.dk>
2024-02-19 18:02:16 +0100

    list

---
+++ list.js
@@ -0,0 +1,13 @@
+window.addEventListener("DOMContentLoaded", init);
+
+const urlParams = new URLSearchParams(window.location.search);
+const query = urlParams.get("season");
+
+fetch("https://lzlmyauwovywksmqbdpd.supabase.co/", {
+  headers: {
+    apikey:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx6bG15YXV3b3Z5d2tzbXFiZHBkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDgwMTA0NzgsImV4cCI6MjAyMzU4NjQ3OH0.ioVoS0GtSqfqHflUCHxA-qPJO-D3_oM-ahW35ZFbDuU",
+  },
+})
+  .then((res) => res.json())
+  .then(console.log);
